91028365eb0681e02b720f0898fba947b26a5968
mander92 <mariomartinrodriguez92@gmail.com>
2024-10-14 14:12:47 +0200

    practicando C#

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+public class Example
+{
+    public static void Main()
+    {
+        int Entero = 10;
+        Console.WriteLine($"Mi entero es: {Entero}")
+        //convertir el valor entero a cadena
+        string cadena = Convert.ToString(Entero);
+        Console.WriteLine($"El valor entero convertido a cadena es: {cadena}");
+
+    }
+}
---
+++ practica c#/Program.cs
@@ -0,0 +1,13 @@
+﻿//Ejercicios de tipos de datos 
+
+//Leer los valores
+
+Console.Write("Proporciona tu nombre: ");
+var nombre = Console.ReadLine();
+Console.Write("Proporcina tu edad: ");
+var edad = Convert.ToInt32(Console.ReadLine());
+Console.Write("Proporciona un sueldo: ");
+var sueldo = Convert.ToInt32(Console.ReadLine());
+Console.Write("Eres un empleado de confianza (true/false): ");
+var confianza = Convert.ToBoolean(Console.ReadLine());
+Console.WriteLine($"El nombre es {nombre}, el sueldo es {sueldo}, la edad es {edad}, eres empleado de confianza? {confianza}");
